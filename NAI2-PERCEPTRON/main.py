f7005bd6ec705c790edc06781f965eda28870ff1
a1mond <kotik.slava2012@gmail.com>
2021-03-27 17:23:18 +0100

    added user input for classification

--- NAI2-PERCEPTRON/main.py
+++ NAI2-PERCEPTRON/main.py
@@ -17,10 +17,15 @@ def get_data(path):
     return rows
 
 
+def encode_features(row, has_output=True):
+    row = str_to_float(row, has_output)
+    return row
+
+
 def encode_train_data(array):
     categories = []
     for row in array:
-        row = str_to_float(row)
+        row = encode_features(row)
         categories.append(row[-1])
 
     categories = set(categories)
@@ -47,8 +52,10 @@ def encode_test_data(array, cat1, cat2):
     return array
 
 
-def str_to_float(array):
-    for i in range(0, len(array) - 1):
+def str_to_float(array, has_output=True):
+    to_range = len(array) - 1 if has_output else len(array)
+
+    for i in range(0, to_range):
         array[i] = float(array[i])
     return array
 
@@ -59,7 +66,7 @@ def get_num_of_features(array):
 
 def get_init_weights(array):
     weight_list = []
-    for i in range(0, get_num_of_features(array) - 1):
+    for i in range(0, get_num_of_features(array)):
         weight_list.append(rnd.random())
     # Bias
     weight_list.append(rnd.random())
@@ -69,13 +76,13 @@ def get_init_weights(array):
 
 def calc_output(theta, features, weights, array):
     dot_prod_calc = 0.0
-    for i in range(0, get_num_of_features(array) - 1):
+    for i in range(0, get_num_of_features(array)):
         dot_prod_calc += features[i] * weights[i]
     dot_prod_calc += 1 * weights[-1]
     return 1 if dot_prod_calc >= theta else 0
 
 
-def train_model(train_array, max_iter=100, learning_rate=0.5, max_error=0, theta=0):
+def train_model(train_array, max_iter=100, learning_rate=0.1, max_error=0, theta=0):
     weights = get_init_weights(train_array)
     num_of_features = get_num_of_features(train_array)
 
@@ -85,28 +92,32 @@ def train_model(train_array, max_iter=100, learning_rate=0.5, max_error=0, theta
         for x in train_array:
             output = calc_output(theta, x, weights, train_array)
             modifier = x[-1] - output
-            for i in range(num_of_features - 1):
+            for i in range(num_of_features):
                 weights[i] += learning_rate * modifier * x[i]
             error += math.fabs(modifier)
         iteration += 1
-        print(f"ITER: {iteration} ERROR: {error/len(train_array)}")
+        print(f"ITER: {iteration} ERROR: {error / len(train_array)}")
         if error <= max_error:
             break
     return weights
 
 
 def test_model(weights, train_set, theta):
+    acc = 0
+
     for x in train_set:
         output = calc_output(theta, x, weights, train_set)
 
         if output == x[-1]:
+            acc += 1
             print(f"BANG {x} = {output}")
         else:
             print(f"    DAMN {x} = {output}")
+    return int((acc / len(train_set)) * 100)
 
 
 if __name__ == '__main__':
-    threshold = 10
+    threshold = 0
 
     train_data = get_data('perceptron.data')
     train_data, c1, c2 = encode_train_data(train_data)
@@ -116,4 +127,15 @@ if __name__ == '__main__':
 
     test_data = get_data('perceptron.test.data')
     test_data = encode_test_data(test_data, c1, c2)
-    test_model(model, train_data, threshold)
+    accuracy = test_model(model, train_data, threshold)
+
+    print(f'\nAccuracy is {accuracy}%')
+    print('Type in your vector for classification in form "x1,x2,etc"')
+    print(f'No more or less than {get_num_of_features(train_data)} features')
+    vector = input()
+    vector = vector.split(',')
+    vector = encode_features(vector, has_output=False)
+
+    v_calc = calc_output(threshold, vector, model, train_data)
+    v_cat = c1.name if v_calc == c1.num else c2.name
+    print(f'Your vector was classified as {v_cat}')
